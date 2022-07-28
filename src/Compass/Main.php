896ad18c936efb3d4775f63af265da99a2cf56f0
itoozh <lkaqi2003a@gmail.com>
2022-07-28 15:30:50 -0500

    terminado.

--- src/Compass/Main.php
+++ src/Compass/Main.php
@@ -2,7 +2,93 @@
 
 namespace Compass;
 
-class Main
-{
+use bossbar\BossBar;
+use bossbar\BossBarTracker;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\utils\TextFormat;
 
+class Main extends PluginBase implements Listener
+{
+    /** @var BossBar[] */
+    private array $bossbars = [];
+    /** @var string[] */
+    private $bars;
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        BossBarTracker::init($this);
+        $this->bars = new \SplFixedArray(360 + 1);
+
+        $bar = [];
+        foreach([
+                    "&8|&r&b&lN&r&8|&r",
+                    "&8|&r&b&lNE&r&8|&r",
+                    "&8|&r&b&lE&r&8|&r",
+                    "&8|&r&b&lSE&r&8|&r",
+                    "&8|&r&b&lS&r&8|&r",
+                    "&8|&r&b&lNW&r&8|&r",
+                    "&8|&r&b&lW&r&8|&r",
+                    "&8|&r&b&lNW&r&8|&r",
+                ] as $dir) {
+            $bar[] = TextFormat::colorize($dir);
+            for($i = 0; $i < 44; $i++) {
+                $bar[] = TextFormat::colorize("&7|&r");
+            }
+        }
+        for($yaw = 0; $yaw <= 360; $yaw++) {
+            $this->bars[$yaw] = implode("", array_slice(Utils::array_shift_circular($bar, // offset it a bit for accuracy... 1/2 of display width seems to do the job well
+                -($yaw - (75 / 2))), 0, 75));
+        }
+    }
+
+    /**
+     * @param PlayerMoveEvent $ev
+     *
+     * @priority        HIGHEST
+     * @ignoreCancelled true
+     */
+    public function onMove(PlayerMoveEvent $ev): void {
+        $p = $ev->getPlayer();
+        //$p->sendPopup("YAW: " . ceil($p->getYaw()) . " DIRECTION: " . $p->getDirection());
+        if(isset($this->bossbars[($k = $p->getName())])) {
+            ($bb = $this->bossbars[$k])->setTitle($this->renderFor($p), false);
+            $bb->updateFor($p);
+        }
+    }
+
+    public function renderFor(Player $p): string {
+        $yaw = (int)ceil(($p->getLocation()->getYaw() + 180) % 360);
+        if($yaw < 0) {
+            $yaw += 360;
+        }
+        return $this->bars[$yaw];
+    }
+
+    public function onJoin(PlayerJoinEvent $ev): void {
+        $p = $ev->getPlayer();
+            $this->showBossbarTo($p);
+    }
+
+    public function showBossbarTo(Player $p): void {
+        $bb = $this->bossbars[$p->getName()] = new BossBar($this->renderFor($p));
+        $bb->showTo($p, false);
+    }
+
+    public function onLeave(PlayerQuitEvent $ev): void {
+        $p = $ev->getPlayer();
+        if(isset($this->bossbars[$p->getName()])) {
+            $this->hideBossbarFrom($p);
+        }
+    }
+
+    public function hideBossbarFrom(Player $p): void {
+        $this->bossbars[($k = $p->getName())]->hideFrom($p);
+        unset($this->bossbars[$k]);
+    }
 }
---
+++ src/bossbar/BossBar.php
@@ -0,0 +1,180 @@
+<?php
+
+/***
+ *
+ *    _____             ____        ____
+ *   / ___/__  ______  / __ \_   __/ __ \
+ *   \__ \/ / / / __ \/ /_/ / | / / /_/ /
+ *  ___/ / /_/ / / / / ____/| |/ / ____/
+ * /____/\__, /_/ /_/_/     |___/_/
+ *      /____/
+ *
+ * Copyright (C) SynPvP - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Ralph B <mcpe4life62@gmail.com>,  September 14, 2018
+ *
+ */
+
+declare(strict_types=1);
+
+namespace bossbar;
+
+
+use InvalidArgumentException;
+use JetBrains\PhpStorm\Pure;
+use pocketmine\entity\Entity;
+use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\network\mcpe\protocol\BossEventPacket;
+use pocketmine\network\mcpe\protocol\RemoveActorPacket;
+use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
+use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
+use pocketmine\network\mcpe\protocol\types\entity\FloatMetadataProperty;
+use pocketmine\network\mcpe\protocol\types\entity\LongMetadataProperty;
+use pocketmine\player\Player;
+
+final class BossBar {
+	/** @var int */
+	protected $entityId;
+	/** @var array */
+	protected $metadata = [];
+	/** @var Player[] */
+	protected $viewers = [];
+	/** @var string */
+	protected $title;
+	/** @var float */
+	protected $value = 1;
+	/** @var bool */
+	protected $titleChanged = false;
+	/** @var bool */
+	protected $valueChanged = false;
+
+	public function __construct(string $title = "BossBar") {
+		if(!BossBarTracker::isInitialized()) {
+			throw new \RuntimeException(BossBarTracker::class . " has not yet been initialized. Please call " . BossBarTracker::class . "::init() before using any boss bar.");
+		}
+		$this->title = $title;
+
+		$this->metadata = [
+			EntityMetadataProperties::FLAGS => new LongMetadataProperty((1 << EntityMetadataFlags::SILENT) | (1 << EntityMetadataFlags::INVISIBLE)),
+			EntityMetadataProperties::BOUNDING_BOX_WIDTH => new FloatMetadataProperty(0),
+			EntityMetadataProperties::BOUNDING_BOX_HEIGHT => new FloatMetadataProperty(0),
+		];
+		$this->regenerateEntityId();
+	}
+
+	public function regenerateEntityId(): void {
+		$this->entityId = Entity::nextRuntimeId();
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getEntityId(): int {
+		return $this->entityId;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getValue(): float {
+		return $this->value;
+	}
+
+	public function setValue(float $value, bool $update = true): void {
+		if($value < 0 || $value > 1) {
+			throw new InvalidArgumentException("Value must be between the range: (0 - 1)");
+		}
+		$this->value = $value;
+		$this->valueChanged = true;
+		if($update) {
+			$this->updateForAll();
+		}
+	}
+
+	public function updateForAll(): void {
+		foreach($this->viewers as $player) {
+			$this->updateFor($player, true);
+		}
+		$this->titleChanged = false;
+		$this->valueChanged = false;
+	}
+
+	public function updateFor(Player $player, bool $batched = false) {
+		if($this->titleChanged) {
+			$player->getNetworkSession()->sendDataPacket($this->createBossEventPacket(BossEventPacket::TYPE_TITLE));
+		}
+		if($this->valueChanged) {
+			$player->getNetworkSession()->sendDataPacket($this->createBossEventPacket(BossEventPacket::TYPE_HEALTH_PERCENT));
+		}
+		if(!$batched) {
+			$this->titleChanged = false;
+			$this->valueChanged = false;
+		}
+	}
+
+	#[Pure] private function createBossEventPacket(int $eventType): BossEventPacket {
+		$pk = new BossEventPacket(); // stuff isn't encoded in if it's not needed by the event type... tnx PM
+		$pk->bossActorUniqueId = $this->entityId;
+		$pk->eventType = $eventType;
+
+		$pk->title = $this->getTitle();
+		$pk->healthPercent = $this->value;
+
+		$pk->color = $pk->overlay = $pk->unknownShort = 0;
+
+		return $pk;
+	}
+
+	public function getTitle(): string {
+		return $this->title;
+	}
+
+	public function setTitle(string $title, bool $update = true) {
+		$this->title = $title;
+		$this->titleChanged = true;
+		if($update) {
+			$this->updateForAll();
+		}
+	}
+
+	/**
+	 * @param Player $p
+	 *
+	 * @return bool
+	 */
+	public function isViewer(Player $p): bool {
+		return in_array($p, array_values($this->viewers), true);
+	}
+
+	public function showTo(Player $player, bool $isViewer = true) {
+		$pk = new AddActorPacket();
+		$pk->actorRuntimeId = $this->entityId;
+		$pk->actorUniqueId = $pk->actorRuntimeId;
+		$pk->type = EntityIds::SLIME;
+		$pk->metadata = $this->metadata;
+		$pk->position = $player->getPosition()->subtract(0, 28, 0); // spawn under the player
+
+		($ns = $player->getNetworkSession())->sendDataPacket($pk);
+		$ns->sendDataPacket($this->createBossEventPacket(BossEventPacket::TYPE_SHOW));
+
+		if($isViewer) {
+			$this->viewers[$player->getName()] = $player;
+		}
+		BossBarTracker::getInstance()->linkBossBar($player, $this);
+	}
+
+	public function hideFrom(Player $player) {
+		$pk = new RemoveActorPacket();
+		$pk->actorUniqueId = $this->entityId;
+
+		($ns = $player->getNetworkSession())->sendDataPacket($this->createBossEventPacket(BossEventPacket::TYPE_HIDE));
+		$ns->sendDataPacket($pk);
+
+		if(isset($this->viewers[$player->getName()])) {
+			unset($this->viewers[$player->getName()]);
+		}
+		BossBarTracker::getInstance()->unlinkBossBar($player, $this);
+	}
+}
